97e0926d99c7273b7c898b96fe7a6d2e84335948
Rohan Ahmad <rohanoahmad@gmail.com>
2016-07-26 20:00:57 +0800

    Added voltage changing mode threshold

--- rainforest.ino
+++ rainforest.ino
@@ -18,30 +18,37 @@
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-#define NUM_LED 15
+#define NUM_LED   15
+#define VOLT_PIN  A0
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
 uint8_t currentLED = 0;
+int blinkTime = 1000;
 
 void allLEDOff(){
-  for (uint8_t pwmnum=0; pwmnum < 16; pwmnum++) {
+  for (uint8_t pwmnum=0; pwmnum < NUM_LED; pwmnum++) {
     pwm.setPWM(pwmnum, 4095,4095 );
   }
 }
 
 
-void setup() {
+void allLEDOn(){
+  for (uint8_t pwmnum=0; pwmnum < NUM_LED; pwmnum++) {
+    pwm.setPWM(pwmnum, 0,4095 );
+  }
+}
 
+
+
+void setup() {
+  
   Serial.begin(9600);
-  Serial.println("16 channel PWM test!");
+  //Serial.println("16 channel PWM test!");
 
   pwm.begin();
   pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
   allLEDOff();
-  // if you want to really speed stuff up, you can go into 'fast 400khz I2C' mode
-  // some i2c devices dont like this so much so if you're sharing the bus, watch
-  // out for this!
 
 
 }
@@ -50,17 +57,32 @@ uint8_t randomLED(){
   uint8_t led = random(NUM_LED);
   while(currentLED==led){
     led = random(NUM_LED);
-    Serial.println(led);
   }
-  Serial.println(led);
+  //Serial.println(led);
   currentLED = led;
   return led;
 }  
+
+
+int getVoltage(){
+  int voltage = analogRead(VOLT_PIN);
+  return voltage;
+}
+
+
+bool voltageRaised(){
+  if(getVoltage() > 100)
+    return true;
+  else
+    return false;
+}
 
 void turnOnLED(uint8_t led, int sleep){
   for(uint16_t i=0; i < 4096; i+=8){
 
     pwm.setPWM(led, 0,i );
+    if(voltageRaised())
+      break;
     delay(sleep);
   }  
 }
@@ -69,17 +91,51 @@ void turnOnLED(uint8_t led, int sleep){
 
 void turnOffLED(uint8_t led, int sleep){
   for(uint16_t i=0; i < 4096; i+=8){
-    pwm.setPWM(led, i,4095 );
+    pwm.setPWM(led, i,4095);
+    if(voltageRaised())
+      break;
+  
     delay(sleep);
   }  
 }
 
 
-
 void loop() {
-  uint8_t led = randomLED();
+  int voltage = getVoltage();
+  Serial.println(voltage);
+  if(voltage < 100){
+    // Random fairy light mode
+    uint8_t led = randomLED(); 
+    turnOnLED(led,1);
+    delay(1000);
+    turnOffLED(led,10);
+
+  }else{
+    if(voltage < 512){
+      blinkTime = map(voltage, 0, 512, 700,300);
+
+    }else{
+      blinkTime = map(voltage, 512, 1024, 300,10);
+
+    }
+    
+    allLEDOn();
+    delay(blinkTime);
+    allLEDOff();
+    delay(blinkTime);
+    
+  }
+  
+/*  uint8_t led = randomLED();
   turnOnLED(led,1);
   delay(1000);
   turnOffLED(led,10);
+
+  allLEDOn();
+  delay(1000);
+  allLEDOff();
+*/  
+//  delay(500);
+
 }
 
